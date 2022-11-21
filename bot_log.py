7f11ded1afd7ae9d5f6ba445bfc74e3a2708cc85
Vladislav Korolev <467098@gmail.com>
2022-11-21 03:51:51 +0300

    Telegram Bot

---
+++ bot_log.py
@@ -0,0 +1,22 @@
+import datetime
+
+def log(text, event = 'Отладочное сообщение'):
+    # Переменная для хранения текущей даты и времени
+    now = datetime.datetime.now()
+    # Отформатированная дата вида: [21.11.2022 02:31:36] 
+    currentDate = now.strftime("%d.%m.%Y %H:%M:%S")
+    
+    with open('bot.log', 'a+', encoding="utf-8") as logFile:
+
+        if event == 'GET' or event == 'POST':
+            logMEssage = f'[{currentDate}] [{event}]\n{text}'
+            logFile.write('\n' + logMEssage)
+
+        elif event == 'СООБЩЕНИЕ':
+            logMEssage = f'[{currentDate}] [{event}]\n{text}'
+            logFile.write('\n' + logMEssage)
+            
+        else:
+            logMEssage = f'[{currentDate}] [{event}] {text}'
+            logFile.write('\n' + logMEssage)
+
